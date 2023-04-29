690d17381c903002ad9e11475c0f63ff6b16bb50
dansever <dansever96@gmail.com>
2023-04-29 12:45:02 +0300

    Files

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -6,6 +6,8 @@ import ItemCard from "../../components/Card/Card"
 import { Link } from "react-router-dom"
 import MainFeed from "../../components/Feed/Feed"
 import Footer from "../../components/Footer/Footer"
+import SearchBar from "../../components/SearchBar/SearchBar";
+import {FilterButton} from "../../components/Buttons/Button";
 
 
 export default function Search() {
@@ -14,6 +16,8 @@ export default function Search() {
             <Header_Search/>
             <div className="search-container">
                 <h1>Explore second hand items!</h1>
+                <SearchBar/>
+                <FilterButton/>
                 <MainFeed/>
             </div>
             <Footer/>
