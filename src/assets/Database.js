3cdf9485e2ff1a6e952c016dee2b19cffb809e41
dansever <dansever96@gmail.com>
2023-04-29 17:52:11 +0300

    product page

--- src/assets/Database.js
+++ src/assets/Database.js
@@ -1,169 +1,48 @@
-import men_black_shirt from "./images/Men's Black T-Shirt.jpg";
-import woman_blue_jeans from "./images/Women's Blue Jeans.jpg";
-import men_brown_leather_jacket from "./images/Men's Brown Leather Jacket.jpg";
-import woman_red_heels from "./images/Women's Red High Heels.jpg";
-import men_blue_polo_shirt from "./images/Men's Blue Polo Shirt.jpg";
-import woman_black_leather_jacket from "./images/Women's Black Leather Jacket.jpg";
-import men_grey_hoodie from "./images/Men's Grey Hoodie.jpg";
-import woman_white_tshirt from "./images/Women's White T-Shirt.jpg";
-import men_blue_denim_jacket from "./images/Men's Blue Denim Jacket.jpg";
-import woman_black_turtleneck_sweater from "./images/Women's Black Turtleneck Sweater.jpg";
-import men_grey_sweatpants from "./images/Men's Grey Sweatpants.jpg";
-import woman_black_jacket from "./images/Women's Black Jacket.jpg";
-import men_black_leather_belt from "./images/Men's Black Leather Belt.jpg";
+import i1 from "./images/i1.jpg"
 
-const products = [
+const data = [
     {
-        "product_id": 1,
-        "img": men_black_shirt,
-        "title": "Men's Black T-Shirt",
-        "description": "A classic black t-shirt for men.",
-        "size": "M",
-        "gender": "Men",
-        "brand": "Nike",
-        "condition": "New",
-        "price": 20,
-        "seller_id": 12345
+        id: 1,
+        image: './images/i1.jpg',
+        title: 'i1',
+        desc: 'Lorem ipsum',
+        type: 'shirt'
     },
     {
-        "product_id": 2,
-        "img": woman_blue_jeans,
-        "title": "Women's Blue Jeans",
-        "description": "A pair of blue jeans for women.",
-        "size": "S",
-        "gender": "Women",
-        "brand": "Levi's",
-        "condition": "Used",
-        "price": 30,
-        "seller_id": 67890
+        id: 2,
+        image: './images/i2.jpg',
+        title: 'i2',
+        desc: 'Lorem ipsum',
+        type: 'shirt'
     },
     {
-        "product_id": 3,
-        "img": men_brown_leather_jacket,
-        "title": "Men's Brown Leather Jacket",
-        "description": "A stylish brown leather jacket for men.",
-        "size": "L",
-        "gender": "Men",
-        "brand": "Tommy Hilfiger",
-        "condition": "New",
-        "price": 150,
-        "seller_id": 54321
+        id: 3,
+        image: './images/i3.jpg',
+        title: 'i3',
+        desc: 'Lorem ipsum',
+        type: 'shirt'
     },
     {
-        "product_id": 4,
-        "img": woman_red_heels,
-        "title": "Women's Red High Heels",
-        "description": "A pair of red high heels for women.",
-        "size": "9",
-        "gender": "Women",
-        "brand": "Steve Madden",
-        "condition": "New",
-        "price": 50,
-        "seller_id": 98765
+        id: 4,
+        image: './images/i4.jpg',
+        title: 'i4',
+        desc: 'Lorem ipsum',
+        type: 'shirt'
     },
     {
-        "product_id": 5,
-        "img": men_blue_polo_shirt,
-        "title": "Men's Blue Polo Shirt",
-        "description": "A comfortable blue polo shirt for men.",
-        "size": "XL",
-        "gender": "Men",
-        "brand": "Ralph Lauren",
-        "condition": "Used",
-        "price": 25,
-        "seller_id": 24680
+        id: 5,
+        image: './images/i5.jpg',
+        title: 'i5',
+        desc: 'Lorem ipsum',
+        type: 'shirt'
     },
     {
-        "product_id": 6,
-        "img": woman_black_leather_jacket,
-        "title": "Women's Black Leather Jacket",
-        "description": "A stylish black leather jacket for women.",
-        "size": "M",
-        "gender": "Women",
-        "brand": "Zara",
-        "condition": "New",
-        "price": 100,
-        "seller_id": 13579
+        id: 6,
+        image: './images/i6.jpg',
+        title: 'i6',
+        desc: 'Lorem ipsum',
+        type: 'shirt'
     },
-    {
-        "product_id": 7,
-        "img": men_grey_hoodie,
-        "title": "Men's Grey Hoodie",
-        "description": "A comfortable grey hoodie for men.",
-        "size": "L",
-        "gender": "Men",
-        "brand": "Adidas",
-        "condition": "Used",
-        "price": 35,
-        "seller_id": 86420
-    },
-    {
-        "product_id": 8,
-        "img": woman_white_tshirt,
-        "title": "Women's White T-Shirt",
-        "description": "A classic white t-shirt for women.",
-        "size": "S",
-        "gender": "Women",
-        "brand": "H&M"
-    },
-    {
-        "product_id": 9,
-        "img": men_blue_denim_jacket,
-        "title": "Men's Blue Denim Jacket",
-        "description": "A classic blue denim jacket for men.",
-        "size": "L",
-        "gender": "Men",
-        "brand": "Levi's",
-        "condition": "New",
-        "price": 100,
-        "seller_id": 12345
-    },
-    {
-        "product_id": 10,
-        "img": woman_black_turtleneck_sweater,
-        "title": "Women's Black Turtleneck Sweater",
-        "description": "A cozy black turtleneck sweater for women.",
-        "size": "M",
-        "gender": "Women",
-        "brand": "Aritzia",
-        "condition": "Used",
-        "price": 50,
-        "seller_id": 67890
-    },
-    {
-        "product_id": 11,
-        "img": men_grey_sweatpants,
-        "title": "Men's Grey Sweatpants",
-        "description": "A comfortable pair of grey sweatpants for men.",
-        "size": "XL",
-        "gender": "Men",
-        "brand": "Nike",
-        "condition": "New",
-        "price": 40,
-        "seller_id": 54321
-    },
-    {
-        "product_id": 12,
-        "img": woman_black_jacket,
-        "title": "Women's Black Jacket",
-        "description": "A stylish black leather jacket for women.",
-        "size": "S",
-        "gender": "Women",
-        "brand": "AllSaints",
-        "condition": "Used",
-        "price": 150,
-        "seller_id": 98765
-    },
-    {
-        "product_id": 13,
-        "img": men_black_leather_belt,
-        "title": "Men's Black Leather Belt",
-        "description": "A classic black leather belt for men.",
-        "size": "32",
-        "gender": "Men",
-        "brand": "Calvin Klein",
-        "condition": "New",
-        "price": 30,
-        "seller_id": 24680
-    }
 ]
+
+export default data
