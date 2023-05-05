0097c076948438c0b30c9e5d04e38941e6c77100
dansever <dansever96@gmail.com>
2023-05-05 18:48:13 +0300

    Cart Page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from "react";
 import {BrowserRouter , Routes, Route} from 'react-router-dom';
 import Home from "./pages/Home";
 import About from "./pages/About/About";
-import Cart from "./pages/Cart/Cart";
-import Contact from "./pages/Contact/Contact";
+import Cart from "./pages/Cart";
+import Contact from "./pages/Contact";
 import Upload from "./pages/Upload";
-import Profile from "./pages/Profile";
 import Product from "./pages/Product";
 import Login from "./pages/Login";
-import Profile_Seller from "./pages/Profile/Profile_Seller";
 import Profile_MyShop from "./pages/Profile/Profile_MyShop";
 import Profile_Saved from "./pages/Profile/Profile_Saved";
+import Profile_Seller from "./pages/Profile/Profile_Sellers";
 import Welcome from "./pages/Welcome";
 import SignUp from "./pages/SignUp";
 
@@ -24,16 +23,12 @@ export default function App() {
                 <Route path="/Signup" element={<SignUp/>}/>
                 <Route path="/Home" element={<Home/>}/>
                 <Route path="/About" element={<About/>}/>
-
-
-
                 <Route path="/Search/:id" element={<Product/>}/>
                 <Route path="/Upload" element={<Upload/>}/>
                 <Route path="/Cart" element={<Cart/>}/>
                 <Route path="/Contact" element={<Contact/>}/>
-                <Route path="/Profile" element={<Profile/>}/>
-                <Route path="/Profile/Seller" element={<Profile_Seller/>}/>
                 <Route path="/Profile/MyShop" element={<Profile_MyShop/>}/>
+                <Route path="/Profile/Sellers" element={<Profile_Seller/>}/>
                 <Route path="/Profile/Saved" element={<Profile_Saved/>}/>
             </Routes>
         </BrowserRouter>
