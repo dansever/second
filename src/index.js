93379beb1b54c126e71d6e4a002d08915a8d2628
dansever <dansever96@gmail.com>
2023-08-05 16:16:39 +0300

    final edits for deployment

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.createRoot(
-    document.getElementById('root'))
-    .render(<App/>);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+
 
 reportWebVitals();
